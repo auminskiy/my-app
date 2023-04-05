979a4702958817511d71941753f3d2aa0cb2fe83
AUminskiy <uminskiyalex@gmail.com>
2023-04-05 18:23:13 +0300

    page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material';
 
+
 const theme = createTheme({
   palette: {
       primary: { 
